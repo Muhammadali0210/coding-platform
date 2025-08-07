4b9b2c2e7be3c1535108c7545e34f29b013073ed
Muhammadali0210 <theprogramer0210@gmail.com>
2025-08-07 21:49:40 +0500

    first commit

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,11 @@
+import { ModeToggle } from "@/components/shared/mode-toggle"
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Click me</Button>
+      <ModeToggle />
+    </div>
+  )
+}
